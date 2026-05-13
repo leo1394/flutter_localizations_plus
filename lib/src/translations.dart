c8373f9ccef3f5067059792092cfa2a0a066b0e4
yuyuelin <yuelin.yu@clobotics.com>
2026-05-13 14:25:56 +0800

    tiny tuned

--- lib/src/translations.dart
+++ lib/src/translations.dart
@@ -99,10 +99,11 @@ class Translations {
   }
 
   /// getter function of all standard locale list of Map filtered by `supported`
-  static get allSupported => _localeSupportedArr;
+  /// Missing type annotation.
+  static List<LocaleConfig> get allSupported => _localeSupportedArr ?? const <LocaleConfig>[];
 
   /// stream subscription for listening language changed in `Translations`
-  static get streamSubscription => controller.stream.listen(null);
+  static StreamSubscription<LocaleConfig> get streamSubscription => controller.stream.listen(null);
 
   /// getter function of fallback locale if no text found in selected locale json file
   static get fallback => _fallback;
